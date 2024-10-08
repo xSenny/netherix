aaabbc7f01f0c76b318fe960f02f67281bd57e05
xSenny <ursuarsenie@gmail.com>
2024-10-08 17:01:31 +0300

    ESlit errors

--- src/components/CollectionLayout.tsx
+++ src/components/CollectionLayout.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { LayoutGrid } from './ui/layout-grid'
 import { Collection } from '../lib/utils'
 
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import { useState } from 'react'
 import { Build, collections } from '../lib/utils'
